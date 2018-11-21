36b06897fb3594de69204a4104040d790b7a5bba
apple39 <apple39.release@gmail.com>
2018-11-21 12:07:03 +0000

    bootstrap design

--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -5,15 +5,18 @@
     <h1>id: {{ $message->id }} のメッセージ編集</h1>
     
     {!! Form::model($message, ['route' => ['messages.update',$message->id],'method' => 'put']) !!}
+        <div class="col-xs-6">
+            <div class="form-group">
+                {!! Form::label('title', 'タイトル') !!}
+                {!! Form::text('title', null, ['class' => 'form-control']) !!}
+            </div>
         
-        {!! Form::label('title', 'タイトル') !!}
-        {!! Form::text('title') !!}
-        
-        {!! Form::label('content','メッセージ:') !!}
-        {!! Form::text('content') !!}
-    
-        {!! Form::submit('更新') !!}
-        
+            <div class="form-group">
+                {!! Form::label('content','メッセージ:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+            {!! Form::submit('更新') !!}
+        </div>
     {!! Form::close() !!}
     
 @endsection
--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -5,13 +5,28 @@
     <h1>メッセージ一覧</h1>
     
     @if (count($messages) > 0)
-        <ul>
+        <table class="table tabel-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タイトル</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+       
             @foreach ($messages as $message)
-                <li>{!! link_to_route('messages.show',$message->id, ['id' => $message->id]) !!} : {{ $message->title }} > {{ $message->content }}</li>
+                <tr>
+                    <td>{!! link_to_route('messages.show',$message->id, ['id' => $message->id]) !!} : {{ $message->title }} > {{ $message->content }}</td>
+                    <td>{{ $message->title }}</td>
+                    <td>{{ $message->cpntent }}</td>
+                    
+                </tr>    
             @endforeach
-        </ul>
+            </tbody>
+        </table>
     @endif
     
-    {!! link_to_route('messages.create','新規のメッセージの投稿') !!}
+    {!! link_to_route('messages.create','新規のメッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
     
 @endsection
